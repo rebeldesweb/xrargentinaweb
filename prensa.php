7995c695a68fb97f220b65daf9e38648fb807304
Franco Benitez <francobenitez980@gmail.com>
2019-11-07 18:30:46 -0300

    cambio de nombre al archivo prensa

--- prensa.php
+++ prensa.php
@@ -1,35 +1,24 @@
-<?php include 'includes/header.html' ?>
+<?php include 'includes/header.html'; ?>
 
 <div class="hero ">
     <picture>
-        <source srcset="https://rebellion.earth/wp/wp-content/uploads/2019/04/donate-Bannerimage.jpg" media="(min-width: 1001px)">
-        <source srcset="https://rebellion.earth/wp/wp-content/uploads/2019/04/donate-Bannerimage-1000x415.jpg" media="(min-width: 601px)">
-        <img src="https://rebellion.earth/wp/wp-content/uploads/2019/04/donate-Bannerimage-600x300.jpg" alt="" />
+       <source srcset="img/press.jpeg" media="(min-width: 1001px)">
+        <source srcset="img/press.jpeg" media="(min-width: 601px)">
+        <img src="img/press.jpeg alt=">
     </picture>
     <h1>Prensa</h1>
 </div>
 
 
-<div class="body-text type container">
+<div class="type container container--bottom0">        
     <div class="featured-text">
         <div class="featured-text__content">
-            <p>Esta es la fuente oficial de información de Extinction Rebellion Argentina</p>
-            <p>Noticias, gacetillas de prensa y actualizaciones </p>
-            <p>Acá podes encontrar <a href="" >videos e imágenes</a></p>
-
-            <p><b>Email:</b> comunicacion@xrargentina.org</p>
-            <p><b>Teléfono:</b></p>
-            <p><b>Redes:</b> comunicacion@xrargentina.org</p>
-
-            <p></p>
-
-
-
-
+            <p>Esta es la fuente oficial de noticias de Extinction Rebellion<br>comunicados de prensa, actualizaciones, <a href="https://drive.google.com/drive/u/0/folders/1OXY4BW177c7ngl-JXue1KC75CU8KU6sR">videos &amp; imágenes</a>. </p>
+            <p class="has-small-font-size"><strong>Email</strong>: <a href="mailto:prensa@xrargentina.org">prensa@xrargentina.org</a></p>
+            <p class="has-small-font-size"><strong>Teléfono</strong>: XX XXXX-XXXX | XX XXXX-XXXX </p>
+            <p class="has-small-font-size"><a href="https://www.facebook.com/XRargentina/">Facebook</a> | <a href="https://twitter.com/XrArgentina">Twitter</a> | <a href="https://www.instagram.com/xrargentina/">Instagram</a> | <a href="https://rebellion.earth/category/videos/">Videos</a></p>
         </div>
     </div>
 </div>
 
-
-           </div><!-- .container -->
 <?php include 'includes/footer2.html' ?>
