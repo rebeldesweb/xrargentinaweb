d1625af3efdbc09aaa93accfb2e52614b26b961f
rebeldesweb-xrj <rebeldesweb-xrj@users.noreply.github.com>
2019-11-12 17:12:48 -0300

    seccion prensa agregada

--- prensa.php
+++ prensa.php
@@ -10,12 +10,11 @@
 </div>
 
 
-<div class="type container container--bottom0">        
+<div class="type container container--bottom0">
     <div class="featured-text">
         <div class="featured-text__content">
             <p>Esta es la fuente oficial de noticias de Extinction Rebellion<br>comunicados de prensa, actualizaciones, <a href="https://drive.google.com/drive/u/0/folders/1OXY4BW177c7ngl-JXue1KC75CU8KU6sR">videos &amp; imágenes</a>. </p>
-            <p class="has-small-font-size"><strong>Email</strong>: <a href="mailto:prensa@xrargentina.org">prensa@xrargentina.org</a></p>
-            <p class="has-small-font-size"><strong>Teléfono</strong>: XX XXXX-XXXX | XX XXXX-XXXX </p>
+            <p class="has-small-font-size"><strong>Email</strong>: <a href="mailto:prensa@xrargentina.org" target="new"  >prensa@xrargentina.org</a></p>
             <p class="has-small-font-size"><a href="https://www.facebook.com/XRargentina/">Facebook</a> | <a href="https://twitter.com/XrArgentina">Twitter</a> | <a href="https://www.instagram.com/xrargentina/">Instagram</a> | <a href="https://rebellion.earth/category/videos/">Videos</a></p>
         </div>
     </div>
